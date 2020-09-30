b453fcc9b313bc1966ebfcd49f839f484d1c0db2
noahHarmuth <flashgordon408@gmail.com>
2020-09-29 23:13:11 -0500

    dsvvWVW

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 app.use((req, res, next) => {
-   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
  });
 // app.get('*', (req, res) => {
 //    res.sendFile(path.join(__dirname,'public', 'index.html'));
